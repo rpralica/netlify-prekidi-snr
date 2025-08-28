db79ebff5a91f3e2b9a1327f182e2adf031d8d16
rpralica <rpralica@gmail.com>
2025-08-28 22:31:10 +0200

    Dodan media querry za responzivost za male ekrane

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 
-        // // Funkcija za učitavanje i aktiviranje navigacije
-        // function loadAndActivateNavbar() {
-        //     fetch('navbar.html')
-        //         .then(response => response.text())
-        //         .then(data => {
-        //             document.getElementById('navbar-placeholder').innerHTML = data;
-
-        //             // Logika za aktiviranje linka
-        //             const currentPath = window.location.pathname; // Dobijamo putanju npr. "/home.html"
-        //             const navLinks = document.querySelectorAll('nav a'); // Dohvatamo sve navigacione linkove
-
-        //             navLinks.forEach(link => {
-        //                 // Proveravamo da li href linka odgovara putanji trenutne stranice
-        //                 // Koristimo .endsWith() jer currentPath može uključivati i root direktorijum
-        //                 if (currentPath.endsWith(link.getAttribute('href'))) {
-        //                     link.classList.add('active'); // Dodajemo klasu 'active'
-        //                 } else {
-        //                     link.classList.remove('active'); // Uklanjamo klasu 'active' sa ostalih
-        //                 }
-        //             });
-        //         })
-        //         .catch(error => console.error('Error loading navbar:', error));
-        // }
-
-        // // Pozivamo funkciju kada se stranica učita
-        // document.addEventListener('DOMContentLoaded', loadAndActivateNavbar);
 
         // Funkcija za učitavanje i aktiviranje navigacije
 function loadAndActivateNavbar() {
